f35995e42d8cc1a5dc1a88a8db1ef4c8651c6803
Gestalte <66849891+Gestalte@users.noreply.github.com>
2023-03-05 12:15:20 +0200

    Add Common project with shared records.

---
+++ AkkaDistribution.Common/Records.cs
@@ -0,0 +1,4 @@
+﻿public sealed record Manifest(DateTime Timesstamp, HashSet<ManifestFile> Files);
+public sealed record ManifestFile(string Filename, string FileHash);
+public sealed record FilePieceDelivery(string Filename, string FileHash, string FilePiece, int TotalPieces, int Position);
+public sealed record MissingPieces(string Filename, string FileHash, List<string[]> MissingPositions);
