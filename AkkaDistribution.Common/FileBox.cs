d468b06361faa85e6ec533451c56eb1525965cb1
Gestalte <66849891+Gestalte@users.noreply.github.com>
2023-03-05 13:47:41 +0200

    Add FileBox

---
+++ AkkaDistribution.Common/FileBox.cs
@@ -0,0 +1,23 @@
+﻿namespace AkkaDistribution.Common
+{
+    public sealed class FileBox
+    {
+        public FileBox(string boxName)
+        {
+            DirectoryPath = Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), boxName)).FullName;
+        }
+
+        public string DirectoryPath { get; private set; }
+
+        public List<string> GetFilesInBox()
+            => Directory.GetFiles(DirectoryPath).ToList();
+
+        public static string? FindFilePath(string filename, FileBox box)
+        {
+            return Directory.GetFiles(box.DirectoryPath)
+                .Where(s => Path.GetFileName(s) == filename)
+                .FirstOrDefault()
+                ?? throw new ArgumentException("File not found in SendBox", nameof(filename));
+        }
+    }
+}
