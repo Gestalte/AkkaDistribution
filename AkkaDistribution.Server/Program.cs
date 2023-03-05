c10e68aa365757b808e0eb3d6b66e583cb13651d
Gestalte <66849891+Gestalte@users.noreply.github.com>
2023-03-06 01:11:31 +0200

    Test server actor.

--- AkkaDistribution.Server/Program.cs
+++ AkkaDistribution.Server/Program.cs
@@ -1,5 +1,12 @@
 ﻿using Akka.Actor;
 using Akka.Configuration;
+using Akka.Util.Internal;
+using AkkaDistribution.Common;
+using AkkaDistribution.Server.Data;
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data;
 
 namespace AkkaDistribution.Server
 {
@@ -7,10 +14,41 @@ namespace AkkaDistribution.Server
     {
         static void Main(string[] args)
         {
+            using (var dbContext = new ServerDbContext())
+            {
+                if (dbContext.Database.GetPendingMigrations().ToList().Count != 0)
+                {
+                    dbContext.Database.Migrate();
+                }
+            }
+
             var configFile = File.ReadAllText("hocon.conf");
             var config = ConfigurationFactory.ParseString(configFile);
 
             var actorSystem = ActorSystem.Create("server-actor-system", config);
+
+            FileBox fileBox = new ("SendFiles");
+
+            IServerDbContextFactory serverDbContextFactory=new ServerDbContextFactory();
+
+            IManifestRepository manifestRepo = new ManifestRepository(serverDbContextFactory);
+            IFilePartDeliveryRepository FilePartDeliveryRepo = new FilePartDeliveryRepository(serverDbContextFactory);
+
+            Props props = Props.Create(() => new FileTransferSupervisor(fileBox, manifestRepo, FilePartDeliveryRepo));
+
+            var fileTransferActor= actorSystem.ActorOf(props,"file-transfer");
+
+            Task<Task<Common.Manifest>> manifest = fileTransferActor.Ask<Task<Common.Manifest>>(new ManifestRequest());
+
+            var x = manifest.Result.Result;
+
+            Console.WriteLine(x.Timestamp);
+            x.Files.ForEach(f => Console.WriteLine(f.FileHash + " - " + f.Filename));
+
+            while (true)
+            {
+                Thread.Sleep(100);
+            }
         }
     }
 }
