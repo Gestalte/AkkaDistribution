75231516bccbcca56d4148b722fafe66dadd2feb
Gestalte <66849891+Gestalte@users.noreply.github.com>
2023-03-06 01:10:56 +0200

    Add logging and handle ManifestRequest in  FileTransferSupervisor.

--- AkkaDistribution.Server/FileTransferSupervisor.cs
+++ AkkaDistribution.Server/FileTransferSupervisor.cs
@@ -1,23 +1,38 @@
 ﻿using Akka.Actor;
 using Akka.Event;
+using Akka.Routing;
 using AkkaDistribution.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using AkkaDistribution.Server.Data;
+using Manifest = AkkaDistribution.Common.Manifest;
 
 namespace AkkaDistribution.Server
 {
-    internal sealed class FileTransferSupervisor:ReceiveActor
+    internal sealed class FileTransferSupervisor : ReceiveActor
     {
+        private readonly FileBox filebox;
+
         private readonly ILoggingAdapter logger = Logging.GetLogger(Context);
 
-        public FileTransferSupervisor()
+        public FileTransferSupervisor
+            (FileBox filebox
+            , IManifestRepository manifestRepository
+            , IFilePartDeliveryRepository filePartDeliveryRepository
+            )
         {
-            Receive<ManifestRequest>(_ => { });
-            Receive<MissingPieces>(msg => { });
-            Receive<Manifest>(msg=> { });
+            this.filebox = filebox;
+
+            Props props = Props
+                .Create(() => new ServerActor(this.filebox, manifestRepository, filePartDeliveryRepository))
+                .WithRouter(new RoundRobinPool(5, new DefaultResizer(1, 1000)));
+
+            var serverActorRouter = Context.ActorOf(props);
+
+            Receive<ManifestRequest>(r =>
+            {
+                Sender.Tell(serverActorRouter.Ask<Common.Manifest>(r));
+            });
+            Receive<MissingPieces>(serverActorRouter.Tell);
+            Receive<Manifest>(serverActorRouter.Tell);
         }
     }
 }
