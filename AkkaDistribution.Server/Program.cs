52ae3f50009d742606e773f2e6c404ac4efb2ad3
Gestalte <66849891+Gestalte@users.noreply.github.com>
2023-03-05 05:39:38 +0200

    Add file hashing method.

--- AkkaDistribution.Server/Program.cs
+++ AkkaDistribution.Server/Program.cs
@@ -8,5 +8,25 @@ namespace MyApp // Note: actual namespace depends on the project name.
         {
             Console.WriteLine("Hello World!");
         }
+
+        public static string[] GetSHA1Hashes(string[] filepaths)
+        {
+            if (filepaths == null || filepaths.Length == 0)
+            {
+                return Array.Empty<string>();
+            }
+
+            var output = new string[filepaths.Length];
+
+            for (int i = 0; i < filepaths.Length; i++)
+            {
+                using SHA1 sha1 = SHA1.Create();
+                using var stream = File.OpenRead(filepaths[i]);
+                byte[] hashBytes = sha1.ComputeHash(stream);
+                output[i] = BitConverter.ToString(hashBytes).Replace("-", "");
+            }
+
+            return output;
+        }
     }
 }
