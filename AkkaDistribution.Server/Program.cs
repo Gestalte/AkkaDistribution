8889fd9bc8f2d2b9a16a84bea57dc58cfd31d58f
Gestalte <66849891+Gestalte@users.noreply.github.com>
2023-03-05 17:27:23 +0200

    Add basic hocon config and read it to create actor system.

--- AkkaDistribution.Server/Program.cs
+++ AkkaDistribution.Server/Program.cs
@@ -1,32 +1,16 @@
-﻿using System;
+﻿using Akka.Actor;
+using Akka.Configuration;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace AkkaDistribution.Server
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            var configFile = File.ReadAllText("hocon.conf");
+            var config=ConfigurationFactory.ParseString(configFile);
 
-        public static string[] GetSHA1Hashes(string[] filepaths)
-        {
-            if (filepaths == null || filepaths.Length == 0)
-            {
-                return Array.Empty<string>();
-            }
-
-            var output = new string[filepaths.Length];
-
-            for (int i = 0; i < filepaths.Length; i++)
-            {
-                using SHA1 sha1 = SHA1.Create();
-                using var stream = File.OpenRead(filepaths[i]);
-                byte[] hashBytes = sha1.ComputeHash(stream);
-                output[i] = BitConverter.ToString(hashBytes).Replace("-", "");
-            }
-
-            return output;
+            var actorSystem = ActorSystem.Create("server-actor-system", config);
         }
     }
 }
