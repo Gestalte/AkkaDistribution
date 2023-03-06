0d1fe8c8321da1c90b65dcdcd937f19abe2317db
Gestalte <66849891+Gestalte@users.noreply.github.com>
2023-03-06 16:28:07 +0200

    Split ManifestActor out of ServerActor.

---
+++ AkkaDistribution.Server/Actors/ManifestActor.cs
@@ -0,0 +1,74 @@
+﻿using Akka.Actor;
+using Akka.Event;
+using Akka.Util.Internal;
+using AkkaDistribution.Common;
+using AkkaDistribution.Server.Data;
+
+namespace AkkaDistribution.Server.Actors
+{
+    internal sealed class ManifestActor : ReceiveActor
+    {
+        private readonly ILoggingAdapter logger = Context.GetLogger();
+
+        public ManifestActor
+            (FileBox filebox
+            , IManifestRepository manifestRepository
+            , IFilePartDeliveryRepository filePartDeliveryRepository
+            )
+        {
+            Receive<ManifestRequest>(_ =>
+            {
+                logger.Info($"Received ManifestRequest");
+
+                var folderManifest = FileHelper.GenerateManifestFromDirectory(filebox);
+
+                logger.Info($"folderManifest: {folderManifest.Timestamp}");
+                folderManifest.Files.ForEach(f => logger.Info($"folderManifest {f.FileHash} - {f.Filename}"));
+
+                Sender.Tell(folderManifest);
+                logger.Info($"Manifest Sent to: " + Sender.Path);
+
+                Self.Tell(new ManifestUpdate(folderManifest));
+            });
+
+            Receive<ManifestUpdate>(m =>
+            {
+                logger.Info($"Received ManifestUpdate");
+
+                var folderManifest = m.Manifest;
+
+                var dbManifest = manifestRepository.GetNewestManifest();
+
+                logger.Info($"dbManifest:  {dbManifest.Timestamp}");
+                dbManifest.Files.ForEach(f => logger.Info($"dbManifest {f.FileHash} - {f.Filename}"));
+
+                var difference = FileHelper.Difference(dbManifest, folderManifest);
+
+                logger.Info($"difference: {difference.Timestamp}");
+                difference.Files.ForEach(f => logger.Info($"difference {f.FileHash} - {f.Filename}"));
+
+                // TODO: If there is a difference, currenty everything is
+                // deleted and recreated, see if there is a better way to do this.
+                if (difference.Files.Count != 0)
+                {
+                    filePartDeliveryRepository.DeleteAllFilePartDeliveries();
+
+                    for (int i = 0; i < folderManifest.Files.Count; i++)
+                    {
+                        var name = folderManifest.Files.ToArray()[i].Filename;
+                        var hash = folderManifest.Files.ToArray()[i].FileHash;
+                        var path = Path.Combine(filebox.DirectoryPath, name);
+
+                        var messages = FileHelper.SplitIntoMessages(path, name, hash);
+
+                        filePartDeliveryRepository.OverwriteFilePartDeliveries(messages);
+                    }
+
+                    manifestRepository.SaveManifest(folderManifest);
+
+                    logger.Info($"Saved manifest to DB.");
+                }
+            });
+        }
+    }
+}
---
+++ AkkaDistribution.Server/Actors/ServerActor.cs
@@ -0,0 +1,31 @@
+﻿using Akka.Actor;
+using Akka.Event;
+using AkkaDistribution.Common;
+using AkkaDistribution.Server.Data;
+
+namespace AkkaDistribution.Server.Actors
+{
+
+    internal sealed class ServerActor : ReceiveActor
+    {
+        private readonly ILoggingAdapter logger = Context.GetLogger();
+
+        public ServerActor
+            (FileBox filebox
+            , IManifestRepository manifestRepository
+            , IFilePartDeliveryRepository filePartDeliveryRepository
+            )
+        {
+            Receive<Common.Manifest>(manifest =>
+            {
+                // TODO: Check that manifest is still valid
+                // TODO: If its invalid, send a new manifest back.
+
+                // TODO: Select from db
+                // TODO: Send to sender.
+            });
+
+            Receive<MissingPieces>(_ => { });
+        }
+    }
+}
