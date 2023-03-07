523e5bc155aed9938a7691d6a43f204b232a7ea1
Gestalte <66849891+Gestalte@users.noreply.github.com>
2023-03-07 02:27:39 +0200

    Remove redundant record

--- AkkaDistribution.Common/Records.cs
+++ AkkaDistribution.Common/Records.cs
@@ -2,7 +2,6 @@
 {
     public sealed record Manifest(DateTime Timestamp, HashSet<ManifestFile> Files);
     public sealed record ManifestFile(string Filename, string FileHash);
-    public sealed record FilePieceDelivery(string Filename, string FileHash, string FilePiece, int TotalPieces, int Position);
     public sealed record MissingPieces(string Filename, string FileHash, List<string[]> MissingPositions);
     public sealed record ManifestRequest();
     public sealed record ManifestUpdate(Manifest Manifest);
