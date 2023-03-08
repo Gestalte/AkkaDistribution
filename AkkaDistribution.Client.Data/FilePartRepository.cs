09fccc6feb9660d07ec36c7ddd98a54281e0b86a
Gestalte <66849891+Gestalte@users.noreply.github.com>
2023-03-08 15:49:53 +0200

    Add project reference to Common for Client data and add FilePartRepository.

---
+++ AkkaDistribution.Client.Data/FilePartRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkkaDistribution.Client.Data
+{
+    internal class FilePartRepository
+    {
+        private readonly IClientDbContextFactory factory;
+
+        public FilePartRepository(IClientDbContextFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        //public int Add(filePartMessage )
+    }
+}
