dc42903fc0c085fb23afb47343f9857882c9b5e3
Gestalte <66849891+Gestalte@users.noreply.github.com>
2023-03-08 17:38:32 +0200

    Split MissingPieces into MissingPieces and MissingPiece.

--- AkkaDistribution.Common/Messages.cs
+++ AkkaDistribution.Common/Messages.cs
@@ -2,7 +2,8 @@
 {
     public sealed record Manifest(DateTime Timestamp, HashSet<ManifestFile> Files);
     public sealed record ManifestFile(string Filename, string FileHash);
-    public sealed record MissingPieces(string Filename, string FileHash, List<string[]> MissingPositions);
+    public sealed record MissingPiece(string Filename, string FileHash, List<int[]> MissingPositions);
+    public sealed record MissingPieces(MissingPiece[] Missing);
     public sealed record ManifestRequest();
     public sealed record ManifestUpdate(Manifest Manifest);
     public sealed record FilePartMessage(string Filename, string FileHash, int TotalPieces, string FilePart, int Position);
