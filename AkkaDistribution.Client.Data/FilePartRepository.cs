d7f9d13d39cb88f32133ed8025aa9a646bdb0db7
Gestalte <66849891+Gestalte@users.noreply.github.com>
2023-03-09 14:55:01 +0200

    Fix GetMissingFilePieces that would break while selecting file parts in FilePartRepository.

--- AkkaDistribution.Client.Data/FilePartRepository.cs
+++ AkkaDistribution.Client.Data/FilePartRepository.cs
@@ -1,4 +1,5 @@
 ﻿using AkkaDistribution.Common;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -59,7 +60,6 @@ namespace AkkaDistribution.Client.Data
             return id;
         }
 
-        // TODO: Check if db file parts match what is described in the manifest.
         public bool CheckThatDbMatchesManifest(Common.Manifest manifest)
         {
             using var context = this.factory.Create();
@@ -84,6 +84,7 @@ namespace AkkaDistribution.Client.Data
             return true;
         }
 
+        // TODO: Think of a better way to do this.
         public Common.MissingPieces GetMissingFilePieces(Common.Manifest manifest)
         {
             using var context = this.factory.Create();
@@ -100,16 +101,27 @@ namespace AkkaDistribution.Client.Data
                 return getMissingPositions(missing);
             };
 
-            MissingPiece[] missingPieces = (from n in context.FileParts
-                                            group n by new { n.Filename, n.FileHash, n.TotalPieces, n.Position } into m
-                                            select new
-                                            {
-                                                FileName = m.Key.Filename,
-                                                m.Key.FileHash,
-                                                MissingPieces = makeMissingPiece(m.Select(s => s.Position).ToArray(), m.Key.TotalPieces),
-                                            })
-                                            .Select(s => new Common.MissingPiece(s.FileName, s.FileHash, s.MissingPieces))
-                                            .ToArray();
+            var groupList = context.FileParts
+                .GroupBy(g => new { Filename = g.Filename, FileHash = g.FileHash })
+                .AsEnumerable()
+                .Select(s => s.GroupBy(g => g.Position))
+                .Select(s => s.Select(s1 => s1.First()).ToList())
+                .ToList();
+
+            List<MissingPiece> missingPieces = new();
+
+            foreach (var group in groupList)
+            {
+                var filename = group.First().Filename;
+                var fileHash = group.First().FileHash;
+                var total = group.First().TotalPieces;
+
+                var positions = group.Select(s => s.Position).ToArray();
+
+                var missing = makeMissingPiece(positions, total);
+
+                missingPieces.Add(new MissingPiece(filename, fileHash, missing));
+            }
 
             List<MissingPiece> output = new();
 
@@ -129,16 +141,25 @@ namespace AkkaDistribution.Client.Data
                         .ToList()
                         .ForEach(f => context.Remove(f));
                 }
-
-                if (missingPiece.MissingPositions.Count > 0)
+                else
                 {
-                    output.Add(missingPiece);
+                    if (missingPiece.MissingPositions.Count > 0)
+                    {
+                        output.Add(missingPiece);
+                    }
                 }
             }
 
             context.SaveChanges();
 
-            return new MissingPieces(output.ToArray());
+            var missingFiles = output
+                .Select(s => (s.Filename, s.FileHash))
+                .ToHashSet()
+                .Except(manifest.Files.Select(s => (s.Filename, s.FileHash)).ToHashSet())
+                .Select(s => new ManifestFile(s.Filename, s.FileHash))
+                .ToArray();
+
+            return new MissingPieces(output.ToArray(), missingFiles);
         }
 
         private static List<int[]> getMissingPositions(int[] missing)
@@ -183,17 +204,23 @@ namespace AkkaDistribution.Client.Data
             return getMissingPositions(missing[newCollection.Count..], collectorSet);
         }
 
-        public string GetFilePiecesByFilenameAndHash(string filename,string fileHash)
+        public string GetFilePiecesByFilenameAndHash(string filename, string fileHash)
         {
             using var context = this.factory.Create();
 
-            return context.FileParts
+            var x = context.FileParts
                 .Where(w => w.Filename == filename)
                 .Where(w => w.FileHash == fileHash)
-                .Select(s => s.Payload)
-                .OrderBy(o => o)
+                .OrderBy(o => o.Position)
+                .Select(s => new Tuple<int, string>(s.Position, s.Payload))
+                .ToList();
+
+            var y = x
                 .Distinct()
-                .Aggregate((a, b) => a + b);
+                .Select(s => s.Item2)
+                .ToList();
+
+            return y.Aggregate((a, b) => a + b);
         }
     }
 }
