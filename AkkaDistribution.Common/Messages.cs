164072a610e064210d9925b64d653f14b9c3453e
Gestalte <66849891+Gestalte@users.noreply.github.com>
2023-03-09 14:53:40 +0200

    Change MissingPieces to include missing files.

--- AkkaDistribution.Common/Messages.cs
+++ AkkaDistribution.Common/Messages.cs
@@ -3,7 +3,7 @@
     public sealed record Manifest(DateTime Timestamp, HashSet<ManifestFile> Files);
     public sealed record ManifestFile(string Filename, string FileHash);
     public sealed record MissingPiece(string Filename, string FileHash, List<int[]> MissingPositions);
-    public sealed record MissingPieces(MissingPiece[] Missing);
+    public sealed record MissingPieces(MissingPiece[] Missing, ManifestFile[] MissingFiles);
     public sealed record ManifestRequest();
     public sealed record ManifestUpdate(Manifest Manifest);
     public sealed record FilePartMessage(string Filename, string FileHash, int TotalPieces, string FilePart, int Position);
