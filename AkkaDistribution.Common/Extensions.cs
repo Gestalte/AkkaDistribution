5790f89069d5d364c92358012a093a2367353c6a
Gestalte <66849891+Gestalte@users.noreply.github.com>
2023-03-14 21:11:07 +0200

    Add Compare extension method to Common.

---
+++ AkkaDistribution.Common/Extensions.cs
@@ -0,0 +1,12 @@
+﻿namespace AkkaDistribution.Common
+{
+    public static class Extensions
+    {
+        public static Manifest Compare(this Manifest oldManifest,Manifest newManifest)
+        {
+            var diff = oldManifest.Files.Except(newManifest.Files).ToHashSet();
+
+            return new Manifest(DateTime.UtcNow, diff);
+        }
+    }
+}
